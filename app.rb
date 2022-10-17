7916154a05a1c625bc20221a6991013cb1cc1abb
kimuit117 <kemoikiuita@gmail.com>
2022-10-17 12:35:02 +0300

    completed assignment

---
+++ app.rb
@@ -0,0 +1,13 @@
+print "Pass this test, please."
+# print "Hello World!"
+# print "Hello World!"
+#displays in block or adds <br>
+
+puts "Hello World!"
+# puts "Hello World!"
+# puts "Hello World!"
+# displays inline no  <br>
+
+# puts [1, 2, 3]
+
+p [1,2,3]
